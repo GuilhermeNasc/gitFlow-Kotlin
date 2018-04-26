48b478f20ac5ecef1ac41949e3dddf2007917f54
Guilherme Nascimento <guilherme.nascimento@livetouch.com.br>
2018-04-26 11:14:04 -0300

    update libs

---
+++ app/src/main/java/com/example/livetouch/hellogitflow/features/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.livetouch.hellogitflow.features
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.livetouch.hellogitflow.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
