beb1478789f309c85bfc72880a6bf61b876e4773
Guilherme Nascimento <guilherme.nascimento@livetouch.com.br>
2018-04-26 11:19:18 -0300

    Init package

---
+++ app/src/main/java/com/example/livetouch/hellogitflow/common/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.livetouch.hellogitflow.common
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.livetouch.hellogitflow.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
