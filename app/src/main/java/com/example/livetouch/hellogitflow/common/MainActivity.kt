2744a2e16c3618cbfc333e2f8c4c0c17168eb9d8
Guilherme Nascimento <guilherme.nascimento@livetouch.com.br>
2018-04-27 11:31:22 -0300

    firebase

--- app/src/main/java/com/example/livetouch/hellogitflow/common/MainActivity.kt
+++ app/src/main/java/com/example/livetouch/hellogitflow/common/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.v("Commit", "outra funcionalidade")
+        Log.v("Commit", "firebase")
     }
 }
